56c8e32163cb7c2e11ec98b744c34aab867bf339
walchko <walchko@users.noreply.github.com>
2019-07-06 17:17:42 +0100

    up

--- calibration/grab.py
+++ calibration/grab.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python
+# 6 July 2019 Kevin Walchko, MIT License
+# Grabs IMU data and stores it to a bag file
 
-from __future__ import division, print_function
 from nxp_imu import IMU
-# from compass import Compass
-from pydar import LDS01
-from the_collector import BagWriter
-# import time
-import platform
-
-system = platform.system()
-
-# lidar serial port
-if system == 'Darwin':
-    port = "/dev/tty.SLAB_USBtoUART"
-elif system == 'Linux':
-    # also valid
-    # /dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0
-    port = "/dev/ttyUSB0"
-else:
-    raise Exception("unsupported OS")
+import time
+from the_collector import BagIt, Pickle
+from collections import namedtuple
 
 """
 nxp_imu
@@ -36,33 +23,61 @@ pi@r2d2 nxp $ sudo i2cdetect -y 1
 70: -- -- -- -- -- -- -- --
 """
 
+class Rate(object):
+    """
+    Uses sleep to keep a desired message/sample rate.
+    """
+    def __init__(self, hertz):
+        self.last_time = time.time()
+        self.dt = 1/hertz
+
+    def sleep(self):
+        """
+        This uses sleep to delay the function. If your loop is faster than your
+        desired Hertz, then this will calculate the time difference so sleep
+        keeps you close to you desired hertz. If your loop takes longer than
+        your desired hertz, then it doesn't sleep.
+        """
+        now = time.time()
+        diff = now - self.last_time
+        if diff < self.dt:
+            new_sleep = self.dt - diff
+            time.sleep(new_sleep)
+
+        # now that we hav slept a while, set the current time
+        # as the last time
+        self.last_time = time.time()
+
+Data = namedtuple('Data', 'data timestamp')
 
 if __name__ == "__main__":
-    bag = BagWriter()
-    # bag.open(['lidar', 'accel', 'mag', 'grav'])
-    # bag.use_compression = True
+    bag = BagIt(Pickle)
 
-    imu = IMU(gs=4, dps=2000, verbose=True)
+    # Open the NXP IMU with:
+    #  accel: 2 g's
+    #  gyros: 250 degrees per sec
+    imu = IMU(gs=2, dps=250)
 
-    lidar = LDS01()
-    lidar.open(port)
-    lidar.run(True)  # why true?
+    rate = Rate(200)
 
     try:
-        for i in range(500):
+        for i in range(10000):
         # while True:
             a, m, g = imu.get()
-            bag.push('accel', list(a))
-            bag.push('mag', list(m))
-            bag.push('gyro', list(g))
+            ts = time.time()
+            bag.push('accel', Data(a,ts))
+            bag.push('mag', Data(m,ts))
+            bag.push('gyro', Data(g,ts))
 
-            pts = lidar.read()
-            bag.push('lidar', pts)
+            # pts = lidar.read()
+            # bag.push('lidar', pts)
 
             # print("[{}] {}".format(i, g))
             # print(type(g))
             if i%20 == 0:
                 print(i)
+
+            rate.sleep()
 
     except Exception as e:
         print(e)
@@ -70,6 +85,6 @@ if __name__ == "__main__":
         pass
 
     # bag.write('data-still.bag')
-    bag.write('data-moving-2.bag')
-    lidar.close()
+    bag.write('data', timestamp=False)
+    # lidar.close()
     print('Done ...')
--- calibration/print-data.py
+++ calibration/print-data.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
+# 6 July 2019 Kevin Walchko, MIT License
+# Just prints data from the bag file
 
-from __future__ import division, print_function
-from the_collector import BagReader
+from the_collector import BagIt, Pickle
+from collections import namedtuple
 
+Data = namedtuple('Data', 'data timestamp')
 
-bag = BagReader()
-data = bag.read('data-still.bag')
+bag = BagIt(Pickle)
+data = bag.read('data.pickle.bag')
 
-for a in data['accel']:
-    print(a)
+start = data['accel'][0].timestamp
+
+def p(header, v, units):
+    print("{:>6}: {:6.1f} {:6.1f} {:6.1f} {}".format(
+        header,
+        v.data[0],
+        v.data[1],
+        v.data[2],
+        units
+    ))
+
+for a, m, g in zip(data['accel'], data['mag'], data['gyro']):
+    print("[{:.3f} sec]--------------------------------".format(a.timestamp-start))
+    p("Accel", a, "[g's]")
+    p("Mag", m, "[uT]")
+    p("Gyro", g, "[rads/sec]")

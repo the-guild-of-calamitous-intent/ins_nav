d92769e6812da0ddb09b308f205cc65efd2c7099
Kevin <walchko@users.noreply.github.com>
2019-07-06 09:03:14 -0400

    Update wgs84.py

--- ins_nav/wgs84.py
+++ ins_nav/wgs84.py
@@ -8,7 +8,7 @@ MU = 3.986004418e14            # Gravitational parameter of Earth
 G0 = 9.7803253359              # Gravity [m/sec^2]
 
 # value?
-class WGS84:
-    re = 6378137.0  # m
-    FLATTENING = 0.00335281066475
-    E2 = 0.00669437999014
+# class WGS84:
+#     re = 6378137.0  # m
+#     FLATTENING = 0.00335281066475
+#     E2 = 0.00669437999014

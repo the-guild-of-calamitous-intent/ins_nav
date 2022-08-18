c1d1239a3bc475ae269d416aec80e227810e15fe
walchko <walchko@users.noreply.github.com>
2022-08-18 09:09:27 -0600

    transform test case

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -9,44 +9,33 @@ lla2ecef([45 90 2000], 'WGS84') => 1e6 * 0.0000    4.5190    4.4888
 
 Can also double check with https://www.ngs.noaa.gov/NCAT/
 """
-# from ins_nav.transforms import ecef2llh, llh2ecef
-# # from ins_nav.transforms import NED, ENU
-# from math import pi, sqrt
-# import numpy as np
-# from numpy.linalg import norm
 
+from ins_nav import *
+import pytest
+import numpy as np
 
-# data = [
-#     # ecef [m,m,m]    lat,lon,alt [deg,deg,m]
-#     # ((5.057590377e6, 2.694861463e6, -2.794229000e6), (-26.145055, 28.050255, 1660.86)), # Pretoria S. Africa
-#     # ((-4.646678571e6, 2.549341033e6, -3.536478881e6), (-33.89115, 151.249186, 86.26)),  # Sydney Australia
-#     ((4510731, 4510731, 0), (0, 45, 999.9564)),      # matlab
-#     # ((0.0000, 4.5190e6, 4.4888e6), (45, 90, 2000)),  # matlab
-# ]
+data = [
+    # ecef [m,m,m]    lat,lon,alt [deg,deg,m]
+    ((5.057590377e6, 2.694861463e6, -2.794229000e6), (-26.145055, 28.050255, 1660.86)), # Pretoria S. Africa
+    ((-4.646678571e6, 2.549341033e6, -3.536478881e6), (-33.89115, 151.249186, 86.26)),  # Sydney Australia
+    ((4510731, 4510731, 0), (0, 45, 999.9564)),      # matlab
+    ((0.0000, 4.5190e6, 4.4888e6), (45, 90, 2000)),  # matlab
+]
 
-# def test_ecef2llh():
-#     for e, ans in data:
-#         f = ecef2llh(*e)
-#         assert norm(f - np.array(ans)) < 0.01, f"ecef2llh {f - ans}"
+def test_local_frames():
+    ned = [500,600,-400]
+    enu = [600,500,400]
 
+    f = NavigationFrame((0,0,0))
 
-# def test_llh2ecef():
-#     for ans, ll in data:
-#         f = llh2ecef(*ll)
-#         assert norm(f - np.array(ans)) < 0.01, f"llh2ecef {f - ans}"
+    assert np.allclose(f.ned2enu(ned), enu)
+    assert np.allclose(f.enu2ned(enu), ned)
 
-# # def test_ned():
-# #     n = NED.from_ll(0,45, 999.9564)
-# #     nn = NED.from_ecef(4510731, 4510731, 0)
-# #
-# #     assert norm(n.origin - nn.origin) < 1
-# #     assert norm(n.R - nn.R) < 0.000001
-# #     assert n.type == nn.type
-# #
-# #     # print(n.origin)
-# #     # print(nn.origin)
-# #     # print(n.R)
-# #     # print(n.R - nn.R)
+def test_ecef():
+    wgs = WGS84()
 
-def test_dummy():
-    assert True
+    for ecef, lla in data:
+        assert np.allclose( wgs.llh2ecef(lla), ecef )
+
+        # the altitude doesn't always come close ... so only look at lat, lon values
+        assert np.allclose( wgs.ecef2llh(ecef)[:2], lla[:2] ), f"{wgs.ecef2llh(ecef)}, {lla}"

4407e78c3133e15beb41e1943902eb7f7fab9d21
walchko <walchko@users.noreply.github.com>
2023-08-29 10:27:53 -0700

    update cpp

---
+++ cpp/src/units.hpp
@@ -0,0 +1,70 @@
+/**************************************\
+ * The MIT License (MIT)
+ * Copyright (c) 2022 Kevin Walchko
+ * see LICENSE for full details
+\**************************************/
+#pragma once
+
+#include <math.h>
+#include <stdint.h>
+
+namespace Units {
+
+// Angular
+constexpr double rad2deg = 180.0f / M_PI;
+constexpr double deg2rad = M_PI / 180.0f;
+constexpr double rps2rpm = 0.0f;     // FIXME
+constexpr double rpm2rps = 0.10472f; // FIXME
+
+// Magnetism
+constexpr double gauss2tesla = 1e-5f;
+constexpr double tesla2gauss = 1e5f;
+
+// Pressure
+constexpr double STD_PRESSURE_PA = 101325.0;                   // Pa
+constexpr double pa2atm = 9.86923e-6f;
+constexpr double atm2pa = 101325.0f;
+
+// Temperature
+inline double f2c(double f) { return 0.5555556f * (f - 32.0f); }
+inline double c2f(double c) { return 1.8f * c + 32.0f; }
+inline double c2k(double c) { return c + 273.15f; }
+inline double k2c(double k) { return k - 273.15f; }
+
+// Distance
+constexpr double m2mi  = 0.000621371f; // meters to miles
+constexpr double mi2m  = 1609.34f;     // miles to meters
+constexpr double km2mi = 0.621371f;
+constexpr double mi2km = 1.60934f;
+constexpr double ft2m  = 0.3048; // feet to meters
+
+// // bits/bytes ... useful?
+// static uint16_t to_b16(const uint8_t b[2], const bool little = true) {
+//   union {
+//     uint16_t s;   // short
+//     uint8_t b[2]; // byte
+//   } buff;
+
+//   if (little) {
+//     buff.b[0] = b[0];
+//     buff.b[1] = b[1];
+//   }
+//   else {
+//     buff.b[0] = b[1];
+//     buff.b[1] = b[0];
+//   }
+
+//   return buff.s;
+// }
+
+/*
+ * Need to fix day for 29, 30, 31 days
+ *
+ * Is this useful?
+ */
+inline double decimal_date(uint8_t yr, uint8_t mon, uint8_t day) {
+  return static_cast<double>(yr) + static_cast<double>(mon) / 12.0f +
+         static_cast<double>(day) / 30.0f;
+}
+
+} // namespace Units
---
+++ cpp/src/wgs84.hpp
@@ -0,0 +1,113 @@
+
+#pragma once
+
+#include <algorithm>
+#include <math.h>
+#include "units.hpp"
+
+#if defined(ARDUINO)
+typedef float nfloat;
+#elif defined(__APPLE__) || defined(__linux__)
+typedef double nfloat;
+#endif
+
+using std::min;
+using std::max;
+
+// namespace Units {
+// constexpr nfloat deg2rad = M_PI/180.0;
+// constexpr nfloat rad2deg = 180.0/M_PI;
+// };
+
+struct llh_t {nfloat lat, lon, h;};
+struct ecef_t {nfloat x, y, z;};
+
+// FIXME: This is a MESS ... what do I want to do?
+
+// class WGS84 {
+//   public:
+namespace WGS84 {
+  constexpr nfloat INV_FLATTENING     = 298.257223563;
+  constexpr nfloat FLATTENING         = 1.0 / INV_FLATTENING;
+  constexpr nfloat SEMI_MAJOR_AXIS_M  = 6378137.0;                  // m
+  constexpr nfloat SEMI_MAJOR_AXIS_KM = SEMI_MAJOR_AXIS_M / 1000.0; // Km
+  // constexpr nfloat STD_PRESSURE_PA    = 101325.0;                   // Pa
+  constexpr nfloat SPIN_RATE_RPS      = 7.2921150e-5;               // rad / sec
+  constexpr nfloat G0                 = 9.7803253359; // Gravity [m/sec^2]
+
+  // nfloat rf = 298.257223563;
+  const nfloat f = 1.0/FLATTENING;
+  const nfloat a = SEMI_MAJOR_AXIS_M;
+  const nfloat b = a - a * f;
+  const nfloat e = sqrt(1.0 - ( (b*b) / (a*a) ));
+  const nfloat r = (2.0*a + b) / 3.0;
+  const nfloat rotation_period = 23*3600 + 56*60 + 4.09053;
+
+  nfloat rate = SPIN_RATE_RPS;  // Rotation rate of Earth [rad/s]
+  nfloat sf = 1.2383e-3;       // Schuller frequency
+
+  nfloat gravity(nfloat lat) {
+    /*
+    Based off the Oxford reference for the gravity formula at sealevel.
+    https://www.oxfordreference.com/view/10.1093/oi/authority.20110803100007626
+
+    Also the WGS84 has a newer model, but it is more computationally
+    intensive and only differs from this one by 0.68 um/s^2
+    https://en.wikipedia.org/wiki/Gravity_of_Earth
+
+    lat: latitude [decimal deg], North is positive and South is negative
+    */
+    lat = lat*Units::deg2rad;
+    return G0*(1.0 + 0.0053024*pow(sin(lat),2) - 0.0000058*pow(sin(2.0*lat),2));
+  }
+
+  ecef_t llh2ecef(const llh_t& llh) {
+    // matlab: https://www.mathworks.com/help/aeroblks/llatoecefposition.html
+    // this works, matches: https://www.oc.nps.edu/oc2902w/coord/llhxyz.htm
+    nfloat lat = llh.lat;
+    nfloat lon = llh.lon;
+    nfloat H = llh.h;
+
+    nfloat mu = lat*Units::deg2rad;
+    nfloat i = lon*Units::deg2rad;
+    nfloat r = 6378137.0;
+    // nfloat f = 1.0/298.257223563;
+    nfloat ls = atan(pow(1.0-f,2) * tan(mu));
+    nfloat rs = sqrt(r*r / (1.0 + (1.0/pow(1.0-f,2) - 1.0)*pow(sin(ls),2)));
+
+    nfloat x = rs*cos(ls)*cos(i) + H*cos(mu)*cos(i);
+    nfloat y = rs*cos(ls)*sin(i) + H*cos(mu)*sin(i);
+    nfloat z = rs*sin(ls)+H*sin(mu);
+
+    return ecef_t{x,y,z};
+  }
+
+  nfloat haversine(const llh_t& a, const llh_t& b) {
+    // Returns the haversine (or great circle) distance between
+    // 2 sets of GPS coordinates. This appears to work really well.
+    //
+    // a: (lat, lon) in decimal deg
+    // b: (lat, lon) in decimal deg
+    const nfloat alat = a.lat*Units::deg2rad;
+    const nfloat blat = b.lat*Units::deg2rad;
+    const nfloat alon = a.lon*Units::deg2rad;
+    const nfloat blon = b.lon*Units::deg2rad;
+    const nfloat dlat = (blat - alat);
+    const nfloat dlon = (blon - alon);
+    const nfloat m = pow(sin(dlat*0.5),2) + cos(alat) * cos(blat) * pow(sin(dlon*0.5),2);
+    return r*2.0*asin(min(1.0, sqrt(m)));
+  }
+
+  /*
+  Returns the geocentric radius based on WGS84
+  lat: latitude in deg
+  */
+  nfloat radius(float lat) {
+    lat *= Units::deg2rad;
+    const nfloat a = SEMI_MAJOR_AXIS_M;
+    const nfloat b = a - a * FLATTENING;
+    const nfloat num = pow(a * a * cos(lat), 2) + pow(b * b * sin(lat), 2);
+    const nfloat den = pow(a * cos(lat), 2) + pow(b * sin(lat), 2);
+    return sqrt(num / den);
+  }
+};
